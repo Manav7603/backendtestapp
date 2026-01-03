c30a04c3f586a4deb404315e8139d62a10917597
Manav7603 <118759375+Manav7603@users.noreply.github.com>
2026-01-04 01:16:36 +0530

    Github data added

--- index.js
+++ index.js
@@ -4,6 +4,41 @@ const express =require('express')
 const app = express()
 const port = 3000
 //Synchronous registration
+const data= {
+  "login": "Manav7603",
+  "id": 118759375,
+  "node_id": "U_kgDOBxQfzw",
+  "avatar_url": "https://avatars.githubusercontent.com/u/118759375?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Manav7603",
+  "html_url": "https://github.com/Manav7603",
+  "followers_url": "https://api.github.com/users/Manav7603/followers",
+  "following_url": "https://api.github.com/users/Manav7603/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Manav7603/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Manav7603/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Manav7603/subscriptions",
+  "organizations_url": "https://api.github.com/users/Manav7603/orgs",
+  "repos_url": "https://api.github.com/users/Manav7603/repos",
+  "events_url": "https://api.github.com/users/Manav7603/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Manav7603/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Manav Pandya",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 33,
+  "public_gists": 0,
+  "followers": 3,
+  "following": 10,
+  "created_at": "2022-11-21T16:57:17Z",
+  "updated_at": "2026-01-03T18:37:58Z"
+}
 app.get('/',(req,res) => {
     res.send('Hello World Manav!!')
 })
@@ -21,6 +56,9 @@ app.get('/hello',(req,res)=>{
     res.send('<h2>hello brothers</h2> ')
 })
 
+app.get('/github', (req,res) => {
+    res.json(data)
+})
 //async
 // Node tells the OS:
 // “Start a server and let me know when it’s ready”
